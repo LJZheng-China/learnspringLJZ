79aa4000914de61b8db44055a28e239cb17d236a
LJZheng <1378094876@qq.com>
2020-03-26 15:56:43 +0800

    first commit

---
+++ src/main/java/org/csu/mypetstoressm/MypetstoressmApplication.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstoressm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoressmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstoressmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/mypetstoressm/MypetstoressmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstoressm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetstoressmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
