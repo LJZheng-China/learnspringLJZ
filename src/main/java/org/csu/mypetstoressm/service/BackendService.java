f918c2938ed44f5ae88b2d5fd9e5f7e9e20a00e5
LJZheng <1378094876@qq.com>
2020-06-14 00:58:07 +0800

    LJZ 后台管理系统实现登录，以及用户信息的初步展示

--- src/main/java/org/csu/mypetstoressm/service/BackendService.java
+++ src/main/java/org/csu/mypetstoressm/service/BackendService.java
@@ -1,4 +1,36 @@
 package org.csu.mypetstoressm.service;
 
+import org.csu.mypetstoressm.domain.Account;
+import org.csu.mypetstoressm.domain.Administrator;
+import org.csu.mypetstoressm.domain.Category;
+import org.csu.mypetstoressm.persistence.AccountMapper;
+import org.csu.mypetstoressm.persistence.AdministratorMapper;
+import org.csu.mypetstoressm.persistence.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BackendService {
+    @Autowired
+    AdministratorMapper administratorMapper;
+    @Autowired
+    AccountMapper accountMapper;
+    @Autowired
+    CategoryMapper categoryMapper;
+
+    public boolean login(Administrator administrator) {
+        return administratorMapper.getAdministratorByUsernameAndPassword(administrator).length == 1;
+    }
+
+    public List<Account> getAccountList() {
+        return accountMapper.getAccountList();
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryMapper.getCategoryList();
+    }
+
+
 }
