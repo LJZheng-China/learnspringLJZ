93b8ca846d71b7fd5218c98e5b84a37ac2b243d9
LJZheng <1378094876@qq.com>
2020-06-14 01:03:59 +0800

    LJZ 后台管理系统实现登录，以及用户信息的初步展示

--- src/main/java/org/csu/mypetstoressm/controller/CMSJsonController.java
+++ src/main/java/org/csu/mypetstoressm/controller/CMSJsonController.java
@@ -1,4 +1,29 @@
 package org.csu.mypetstoressm.controller;
 
+import org.csu.mypetstoressm.domain.Account;
+import org.csu.mypetstoressm.domain.Category;
+import org.csu.mypetstoressm.service.BackendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/CMSJson")
 public class CMSJsonController {
+
+    @Autowired
+    BackendService backendService;
+
+    @RequestMapping("/accountlist")
+    public List<Account> getAccountList() {
+        return backendService.getAccountList();
+    }
+
+    @RequestMapping("/category")
+    public List<Category> getCategoryList() {
+        return backendService.getCategoryList();
+    }
 }
--- src/main/java/org/csu/mypetstoressm/persistence/AdministratorMapper.java
+++ src/main/java/org/csu/mypetstoressm/persistence/AdministratorMapper.java
@@ -1,4 +1,9 @@
 package org.csu.mypetstoressm.persistence;
 
+import org.csu.mypetstoressm.domain.Administrator;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface AdministratorMapper {
+    Administrator[] getAdministratorByUsernameAndPassword(Administrator administrator);
 }
